629c17b66ad8003798f5a65932b909432a9e922c
sunga-lee <sungajunior1@gmail.com>
2023-05-27 10:01:32 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
   # write code here
+  attr_reader :x, :y, :z
+
+  def initialize(x, y, Z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    valid_triandle
+    if x==y && y==Z
+      :equilateral
+    elsif x==y || y==z || x==z 
+      :isosceles
+    else
+      :scalene
+    end
+
+  end
+
+  def sides_greater_than_zero?
+    [x, y, z].all?(&:positive?)
+
+  end
+
+  def valid_triandle_inequality? 
+    x+y > z && x+z > y && y+z > x
+  end
+
+  def valid_triangle
+    raise TriangleError unless sides_greater_than_zero? & valid_triangle_inequality? 
+
+  end
+
+  class TriangleError < StandardError
+  end
 end
